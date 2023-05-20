f0f6d1a90a2398e5f20c860e9a0431e6645e6bc2
abdelaliichou <97667005+abdelaliichou@users.noreply.github.com>
2023-05-20 16:21:11 +0100

    cart deleted

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ const payments = require("./routes/payments");
 const promotions = require("./routes/promotions");
 const suppliers = require("./routes/suppliers");
 const receptions = require("./routes/receptions");
-const carts = require("./routes/carts");
 const brands = require("./routes/brands");
 const app = express();
 const userRoute = require("./routes/UserRoutes");
@@ -41,7 +40,6 @@ app.use("/api/promotions", promotions);
 app.use("/api/brands", brands);
 app.use("/api/suppliers", suppliers);
 app.use("/api/receptions", receptions);
-app.use("/api/carts", carts);
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 
