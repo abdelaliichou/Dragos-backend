af4e5c0595d6be70b8bd8f107fb9577a0b8a097f
abdelaliichou <97667005+abdelaliichou@users.noreply.github.com>
2023-06-07 11:33:06 +0100

    get wish list added

--- src/Utils/validators/UserValidator.js
+++ src/Utils/validators/UserValidator.js
@@ -1,127 +1,123 @@
-const slugify = require('slugify');
-const bcrypt = require('bcrypt');
-const { check, body } = require('express-validator');
-const validatorMiddleware = require('../../middlewares/validationMiddleware');
-const User = require('../../models/userModel');
-
-
-
-
+const slugify = require("slugify");
+const bcrypt = require("bcrypt");
+const { check, body } = require("express-validator");
+const validatorMiddleware = require("../../middlewares/validationMiddleware");
+const User = require("../../models/userModel");
 
 const createUserValidator = [
-  check('name')
+  check("name")
     .notEmpty()
-    .withMessage('User required')
+    .withMessage("User required")
     .isLength({ min: 3 })
-    .withMessage('Too short User name')
+    .withMessage("Too short User name")
     .custom((val, { req }) => {
       req.body.slug = slugify(val);
       return true;
     }),
- 
-  check('email')
+
+  check("email")
     .notEmpty()
-    .withMessage('Email required')
+    .withMessage("Email required")
     .isEmail()
-    .withMessage('Invalid email address')
+    .withMessage("Invalid email address")
     .custom((val) =>
       User.findOne({ email: val }).then((user) => {
         if (user) {
-          return Promise.reject(new Error('E-mail already in user'));
+          return Promise.reject(new Error("E-mail already in user"));
         }
       })
     ),
 
-  check('password')
+  check("password")
     .notEmpty()
-    .withMessage('Password required')
+    .withMessage("Password required")
     .isLength({ min: 6 })
-    .withMessage('Password must be at least 6 characters')
+    .withMessage("Password must be at least 6 characters")
     .custom((password, { req }) => {
       if (password !== req.body.passwordConfirm) {
-        throw new Error('Password Confirmation incorrect');
+        throw new Error("Password Confirmation incorrect");
       }
       return true;
     }),
 
-  check('passwordConfirm')
+  check("passwordConfirm")
     .notEmpty()
-    .withMessage('Password confirmation required'),
+    .withMessage("Password confirmation required"),
 
-  check('phone')
+  check("phone")
     .optional()
-    .isMobilePhone(['ar-DZ', 'ar-SA'])
-    .withMessage('Invalid phone number only accepted Egy and SA Phone numbers'),
+    .isMobilePhone(["ar-DZ", "ar-SA"])
+    .withMessage("Invalid phone number only accepted Egy and SA Phone numbers"),
 
-  check('profileImg').optional(),
-  check('role').optional(),
+  check("profileImg").optional(),
+  check("role").optional(),
 
   validatorMiddleware,
 ];
 
 const getUserValidator = [
-  check('id').isMongoId().withMessage('Invalid User id format'),
+  check("id").isMongoId().withMessage("Invalid User id format"),
   validatorMiddleware,
 ];
 
 const updateUserValidator = [
-  check('id').isMongoId().withMessage('Invalid User id format'),
-  body('name')
+  check("id").isMongoId().withMessage("Invalid User id format"),
+  body("name")
     .optional()
     .custom((val, { req }) => {
       req.body.slug = slugify(val);
       return true;
     }),
-  check('email')
+  check("email")
     .notEmpty()
-    .withMessage('Email required')
+    .withMessage("Email required")
     .isEmail()
-    .withMessage('Invalid email address')
+    .withMessage("Invalid email address")
     .custom((val) =>
       User.findOne({ email: val }).then((user) => {
         if (user) {
-          return Promise.reject(new Error('E-mail already in user'));
+          return Promise.reject(new Error("E-mail already in user"));
         }
       })
     ),
-  check('phone')
+  check("phone")
     .optional()
-    .isMobilePhone(['ar-EG', 'ar-SA'])
-    .withMessage('Invalid phone number only accepted Egy and SA Phone numbers'),
+    .isMobilePhone(["ar-EG", "ar-SA"])
+    .withMessage("Invalid phone number only accepted Egy and SA Phone numbers"),
 
-  check('profileImg').optional(),
-  check('role').optional(),
+  check("profileImg").optional(),
+  check("role").optional(),
   validatorMiddleware,
 ];
 
 const changeUserPasswordValidator = [
-  check('id').isMongoId().withMessage('Invalid User id format'),
-  body('currentPassword')
+  check("id").isMongoId().withMessage("Invalid User id format"),
+  body("currentPassword")
     .notEmpty()
-    .withMessage('You must enter your current password'),
-  body('passwordConfirm')
+    .withMessage("You must enter your current password"),
+  body("passwordConfirm")
     .notEmpty()
-    .withMessage('You must enter the password confirm'),
-  body('password')
+    .withMessage("You must enter the password confirm"),
+  body("password")
     .notEmpty()
-    .withMessage('You must enter new password')
+    .withMessage("You must enter new password")
     .custom(async (val, { req }) => {
       // 1) Verify current password
       const user = await User.findById(req.params.id);
       if (!user) {
-        throw new Error('There is no user for this id');
+        throw new Error("There is no user for this id");
       }
       const isCorrectPassword = await bcrypt.compare(
         req.body.currentPassword,
         user.password
       );
       if (!isCorrectPassword) {
-        throw new Error('Incorrect current password');
+        throw new Error("Incorrect current password");
       }
 
       // 2) Verify password confirm
       if (val !== req.body.passwordConfirm) {
-        throw new Error('Password Confirmation incorrect');
+        throw new Error("Password Confirmation incorrect");
       }
       return true;
     }),
@@ -129,41 +125,37 @@ const changeUserPasswordValidator = [
 ];
 
 const deleteUserValidator = [
-  check('id').isMongoId().withMessage('Invalid User id format'),
+  check("id").isMongoId().withMessage("Invalid User id format"),
   validatorMiddleware,
 ];
 
 const updateLoggedUserValidator = [
-  body('name')
+  body("name")
     .optional()
     .custom((val, { req }) => {
       req.body.slug = slugify(val);
       return true;
     }),
-  check('email')
+  check("email")
     .notEmpty()
-    .withMessage('Email required')
+    .withMessage("Email required")
     .isEmail()
-    .withMessage('Invalid email address')
+    .withMessage("Invalid email address")
     .custom((val) =>
       User.findOne({ email: val }).then((user) => {
         if (user) {
-          return Promise.reject(new Error('E-mail already in user'));
+          return Promise.reject(new Error("E-mail already in user"));
         }
       })
     ),
-  check('phone')
-    .optional()
-    .isMobilePhone(['ar-EG', 'ar-SA'])
-    .withMessage('Invalid phone number only accepted Egy and SA Phone numbers'),
 
   validatorMiddleware,
 ];
-module.exports ={
+module.exports = {
   updateLoggedUserValidator,
   deleteUserValidator,
   changeUserPasswordValidator,
   updateUserValidator,
   getUserValidator,
-  createUserValidator
-}
+  createUserValidator,
+};
--- src/services/wishListService.js
+++ src/services/wishListService.js
@@ -1,5 +1,4 @@
-
-const {Product} = require("../models/product");
+const { Product } = require("../models/product");
 
 const addToWishlist = async (req, res) => {
   try {
@@ -23,29 +22,38 @@ const addToWishlist = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
-const removeFromWishlist = async (req, res) => {
-    try {
-      const { product_id } = req.body;
-  
-      // Check if the product exists
-      const product = await Product.findById(product_id);
-      if (!product) {
-        return res.status(404).json({ error: "Product not found" });
-      }
-  
-      // Remove the product ID from the user's wishlist if it exists
-      const index = req.user.wishlist.indexOf(product_id);
-      if (index !== -1) {
-        req.user.wishlist.splice(index, 1);
-        await req.user.save();
-      }
-  
-      res.json({ message: "Product removed from wishlist" });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
-  };
-  
-module.exports ={ addToWishlist,removeFromWishlist};
 
+const getWishlist = async (req, res) => {
+  try {
+    res.status(200).send(req.user.wishlist);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const removeFromWishlist = async (req, res) => {
+  try {
+    const { product_id } = req.body;
+
+    // Check if the product exists
+    const product = await Product.findById(product_id);
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    // Remove the product ID from the user's wishlist if it exists
+    const index = req.user.wishlist.indexOf(product_id);
+    if (index !== -1) {
+      req.user.wishlist.splice(index, 1);
+      await req.user.save();
+    }
+
+    res.json({ message: "Product removed from wishlist" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+module.exports = { addToWishlist, removeFromWishlist, getWishlist };
